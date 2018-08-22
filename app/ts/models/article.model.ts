07d592e69c165ddea5926265277e77aa78cde7f3
FabienGreard <grd.fabien@gmail.com>
2018-08-22 15:15:57 +0200

    cleanup master

--- app/ts/models/article.model.ts
+++
@@ -1,7 +0,0 @@
-export class Article {
-  id?: string;
-  title?: string;
-  date?: any;
-  content?: any;
-  tags?: any;
-}
--- app/ts/modules/console-log/index.ts
+++
@@ -1,2 +0,0 @@
-export * from './console-log.module';
-export * from './console-log.service';
